eaa0fa777e85a1e7b11df5bbb47cf7139281c2fd
carneiroArleilson <carneiroarleilson@gmail.com>
2021-01-10 22:25:38 -0400

    final primeiro site

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,12 @@
 function github(){
+    window.open('https://github.com/carneiroArleilson');
+}
+
+function cadastrar(){
+    var name = document.querySelector('input#tname').value;
+    var sobreNome = document.querySelector('input#tlname').value;
+    var email = document.querySelector('input#tmail').value;
+    var senha = document.querySelector('input#tsenha').value;
     
+    console.log(name , sobreNome, email, senha);
 }
